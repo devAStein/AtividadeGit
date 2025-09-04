9d0dad2cef3a8791b9bce2dce292a1cdf2d1661f
Matheus-ls1 <166852383+Matheus-ls1@users.noreply.github.com>
2025-09-03 21:15:37 -0300

    Update main.py

--- AtividadeGit/main.py
+++ AtividadeGit/main.py
@@ -1,7 +1,24 @@
 def sao_anagramas(string1, string2):
-  # TODO: implementar a lógica
+
+  string = string1.replace(" ", "").lower()
+  string2 = string2.replace(" ", "").lower()
+
+  if sorted(string) == sorted(string2):
+    return True
+  else:
+    return False
+
   pass
+
+
 
 def cifra_de_cesar(texto, deslocamento):
   # TODO: Implementar a lógica
   pass
+
+
+
+print("------ Teste Função Anagramas ---------")
+print(sao_anagramas("amor", "roma"))            # True
+print(sao_anagramas("A gentleman", "Elegant man"))  # True
+print(sao_anagramas("gato", "cabra"))
